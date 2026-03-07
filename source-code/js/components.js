9503a7e57152a986d01f0cba79322f3959263a22
Devin-Diaz <diazdevin7@outlook.com>
2026-03-06 21:21:06 -0500

    initial website prototype

---
+++ source-code/js/components.js
@@ -0,0 +1,37 @@
+document.getElementById("navbar").innerHTML = `
+
+<header class="navbar">
+
+<div class="nav-left">
+<a href="index.html" class="site-title">AI in CS Education</a>
+</div>
+
+<div class="nav-middle">
+<div class="nav-image">
+    <img src="media/navbar_img.webp" alt="Navbar image">
+</div>
+</div>
+
+<div class="nav-right">
+
+<a href="index.html">Home</a>
+<a href="Page_1.html">Page 1</a>
+<a href="Page_2.html">Page 2</a>
+<a href="Page_3.html">Page 3</a>
+
+</div>
+
+</header>
+
+`;
+
+
+document.getElementById("footer").innerHTML = `
+
+<footer class="footer">
+
+<p>AI in CS Education | Intro CS Project</p>
+
+</footer>
+
+`;
