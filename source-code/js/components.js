01067e5296e3ad20dd6671d17e9d3d6ef29684cd
treed-3 <tyler_a_reed@outlook.com>
2026-03-30 17:55:22 -0400

    Update survey link titles for clarity and enhance reference section in multiple HTML files

--- source-code/js/components.js
+++ source-code/js/components.js
@@ -15,8 +15,8 @@ document.getElementById("navbar").innerHTML = `
 <div class="nav-right">
 
 <a href="index.html">Home</a>
-<a href="page1.html">Page 1</a>
-<a href="page2.html">Page 2</a>
+<a href="page1.html">1st Article</a>
+<a href="page2.html">2nd Article</a>
 <a href="takeAction.html">Taking Action</a>
 <a href="aboutUs.html">About Us</a>
 
